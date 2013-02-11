e6e67d97b09621eaf05696eab5d56e03849fd1b1
gterzian <gregory.terzian@gmail.com>
2013-02-12 00:31:59 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from distutils.core import setup
 import async_image_save
 
-long_description = open('README.rst').read()
-
 setup(
     name='django-async-gt',
-    data_files=[('', ['requirements.txt', 'README.rst', 'LICENSE', 'MANIFEST', '.gitignore']),],
-    version='0.2',
+    data_files=[('', ['requirements.txt', 'LICENSE', 'MANIFEST', '.gitignore']),],
+    version='0.4',
     packages=['async_image_save',],
     description='a simple app to asynchronously upload images with Django, using Celery',
-    long_description=long_description,
+    long_description=open('README.rst').read(),
     author='Gregory Terzian',
     author_email='gregory.terzian@gmail.com',
     license='BSD License',
