4efabbc192e6257f0a42acc5dad81e60887807cd
gterzian <gregory.terzian@gmail.com>
2013-02-19 13:48:13 +0100

    changed file into file_info

--- async_image_save/tasks.py
+++ async_image_save/tasks.py
@@ -5,15 +5,15 @@ from celery import task
 import StringIO
 
 @task()
-def async_save(data, file, type, id):
+def async_save(data, file_info, type, id):
     #get the instance
     instance = get_object_or_404(type, pk=id)
     #img is the data of the image read into a StringIO buffer
     img = StringIO.StringIO(data['data'])
     #pass the StringIo, and a list of required args to InMemoryUploadedFile
-    image = InMemoryUploadedFile(img, *file)
+    image = InMemoryUploadedFile(img, *file_info)
     #add the name of the formfield to the instance's __dict__, and set the image to it. 
-    instance.__dict__[file[0]] = image
+    instance.__dict__[file_info[0]] = image
     instance.save()
     #invalidate the cache, this is necessary if you are using Johnny Cache, as adding the image to the instance will not invalidate the cache
     cache.clear()
--- async_image_save/utils.py
+++ async_image_save/utils.py
@@ -6,16 +6,16 @@ def save_image(imagefield_data, instance):
     data = {}
     #file is a list containing all info needed to recontruct a UploadedFile object, in the correct order into which they have to
     #be passed to __init__
-    file = []
+    file_info = []
     #name of the form field
-    file.append(imagefield_data.field_name)
+    file_info.append(imagefield_data.field_name)
     #name of the image
-    file.append(imagefield_data.name)
-    file.append(imagefield_data.content_type)
-    file.append(imagefield_data.size)           
-    file.append(imagefield_data.charset)
+    file_info.append(imagefield_data.name)
+    file_info.append(imagefield_data.content_type)
+    file_info.append(imagefield_data.size)           
+    file_info.append(imagefield_data.charset)
     #the actual data of the image, read into a string
     data['data'] = imagefield_data.read()
             
     #send the image to be saved by a worker
-    async_save.delay(data, file, instance.__class__, instance.pk)
+    async_save.delay(data, file_info, instance.__class__, instance.pk)
